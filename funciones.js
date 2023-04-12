0c45e23672773d2cb2e8e1c6060295cb68ce53e5
maxgcba <126741704+maxgcba@users.noreply.github.com>
2023-04-11 23:31:36 -0300

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,3 @@
+document.getElementById('boton').addEventListener('click', function() {
+    document.getElementById("demo").innerHTML = "Doctora en Ciencias Médicas - Universidad Nacional Córdoba"
+});
